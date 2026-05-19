66b431d1ff33aff952924ec98aedfd42d0742172
YashasVM <yashasvm@users.noreply.github.com>
2026-05-19 13:21:14 +0530

    feat: upgrade stream quality to 1080p@60fps with 20Mbps bitrate

--- android/app/src/main/java/dev/openstream/app/stream/StreamConfig.kt
+++ android/app/src/main/java/dev/openstream/app/stream/StreamConfig.kt
@@ -15,21 +15,21 @@ data class StreamConfig(
         get() = bitrate / 1_000_000
 
     companion object {
-        val Default1080p30 = StreamConfig(
+        val Default1080p60 = StreamConfig(
             width = 1920,
             height = 1080,
-            fps = 30,
-            bitrate = 12_000_000,
+            fps = 60,
+            bitrate = 20_000_000,
             keyframeIntervalSeconds = 1,
             latencyMs = 120,
             codecPreference = CodecPreference.ForceAvc,
         )
 
-        val Fallback720p30 = StreamConfig(
+        val Fallback720p60 = StreamConfig(
             width = 1280,
             height = 720,
-            fps = 30,
-            bitrate = 8_000_000,
+            fps = 60,
+            bitrate = 12_000_000,
             keyframeIntervalSeconds = 1,
             latencyMs = 120,
             codecPreference = CodecPreference.ForceAvc,
